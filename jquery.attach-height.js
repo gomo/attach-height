1123805d0a39b92a980b554b42634bf1500dd66f
Masamoto Miyata <miyata@sincere-co.com>
2013-06-27 09:50:40 +0900

    First implementation.

---
+++ jquery.attach-height.js
@@ -0,0 +1,34 @@
+;(function(){
+
+    $.fn.attachHeight = function(options){
+        var op = $.extend({resizeTimeout: 200}, options);
+
+        if(!op.ratio){
+            throw 'Missing ratio parameter in options. usage: $(".selector").attachHeight({ratio:0.8})';
+        }
+
+        var elems = this;
+        var attach = function(){
+            elems.height(elems.width() * op.ratio);
+        };
+
+        attach();
+
+        var timer = false;
+        $(window).resize(function() {
+            if (timer !== false) {
+                clearTimeout(timer);
+            }
+            timer = setTimeout(function() {
+                attach();
+            }, op.resizeTimeout);
+        });
+
+        window.onorientationchange = function() {
+            setTimeout(function(){
+                attach();
+            }, 0);
+        };
+    };
+
+})(jQuery);
